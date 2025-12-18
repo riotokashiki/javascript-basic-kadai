ab3601e12c6d46b9d6d2603091ab9adc8498efb7
ithachioji2_546 <ithachioji21.546@gmail.com>
2025-12-18 14:47:09 +0900

    good

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,2 +1,26 @@
-let height=document.querySelector('#height').value
-let weight=document.querySelector('#weight').value
+
+let submitButton=document.getElementById("submit")  //fetching the submit button
+submitButton.addEventListener("click",function(event){ // when the submit button is clicked, do the below
+  event.preventDefault();// preventing a page reload
+
+  /////your code goes here///////////////////
+let height=parseFloat(document.querySelector('#height').value)/100
+let weight=parseFloat(document.querySelector('#weight').value)
+
+console.log(`height is ${height}`);
+console.log(`height is ${weight}`);
+
+
+
+
+
+
+
+
+let BMI=(weight/(height*height)).toFixed(1);
+
+document.write(`your BMI is ${BMI}`);
+console.log(BMI);
+
+
+});
